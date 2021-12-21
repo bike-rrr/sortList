6f0539116578d06a053b461fbcb7126912481cb0
bike-rrr <tesla201299@gmail.com>
2021-12-20 19:14:31 -0600

    got adding key working

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const peeps = {
     f: { weight: 'green' },
     g: { weight: 'blue' },
     sh: { weight: 'brewery' },
-    k: { weight: 'calSag' }
+    k: { weight: 'calSag' },
+    test: { weight: "newone" }
 }
 
 const list = {
@@ -24,12 +25,41 @@ const list = {
     red: 6,
     brewery: -1,
     green: 5,
-    blue: 4
-}
+    blue: 4,
+    newone: 5.5
 
+}
+// console.log(Object.entries(peeps))
 const sorted = Object.entries(peeps).sort(([, aVal], [, bVal]) => {
     if (list[aVal.weight] < list[bVal.weight]) { return -1 };
     if (list[aVal.weight] > list[bVal.weight]) { return 1 };
     return 0;
 });
-console.log(sorted);
+
+// function to add new paramater to list in a desired spot
+function place(spot) {
+    // convert spot to index spot => 1 => 0, 10 => 9 and so forth,
+
+    let list = Object.entries(peeps);
+
+    if (spot === 0) {
+        let before = [spot];
+        return before.concat(list)
+    }
+    if (spot < list.length) {
+        let before = list.slice(0, spot);
+        before.push(spot);
+        let after = list.slice(spot);
+        return before.concat(after)
+    }
+    if (spot === list.length) {
+        list.push(spot)
+        return list;
+    }
+
+
+}
+
+for (let i = 0; i < Object.keys(peeps).length + 1; i++) {
+    console.log(place(i))
+}
